d2a883b936f4ee77c041a903301673e10df73cfd
Muhammed <muhammedtoichubai@gmail.com>
2022-02-17 00:47:09 -0600

    practice

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -6,6 +6,58 @@ public abstract class Animal {
     private double weight;
     private String color;
 
+    public Animal() {
+    }
+
+    public Animal(String name, byte age, double weight, String color) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", weight=" + weight +
+                ", color='" + color + '\'' +
+                '}';
+    }
+
     public abstract void eat();
 
     public void voice(){
@@ -15,4 +67,8 @@ public abstract class Animal {
     public void sleep(){
         System.out.println("Sleeping");
     }
+
+
+
+
 }
